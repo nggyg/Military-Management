962df2467d4901a520ce237ab788f6306f8760c2
SofarRadu <sofarradu@yahoo.com>
2022-10-24 17:02:05 +0300

    Vehicle list in Soldier

--- src/basic/Soldier.java
+++ src/basic/Soldier.java
@@ -1,11 +1,26 @@
 package basic;
 
+import java.util.ArrayList;
+
 public class Soldier extends Person{
     private Rang rang;
+
+    private ArrayList<Vehicle> vehicleList;
+
+    public ArrayList<Vehicle> getVehicleList() {
+        return vehicleList;
+    }
+
+    public void setVehicleList(ArrayList<Vehicle> vehicleList) {
+        this.vehicleList = vehicleList;
+    }
+
     public Soldier(String name, int id, Rang rang) {
         super(name, id);
         this.rang=rang;
     }
+
+
 
     public Rang rang() {
         return rang;
