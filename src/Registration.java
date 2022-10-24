368090562a0d5561e8e938d86686f33719adcf01
SofarRadu <sofarradu@yahoo.com>
2022-10-24 23:34:29 +0300

    Beschreibung neterminat

--- src/Registration.java
+++ src/Registration.java
@@ -4,3 +4,18 @@ public class Registration {
     public Registration() {
     }
 }
+
+/*
+    Jeder Soldat - hat eine Rustung, Waffe und Rang
+                 - ist eine Person
+                 - wenn Rang 'Private' ist dann kann auch jeder soldat ein Fahrer fur
+                   ein Vehicle sein
+    Jede Waffe, Rustung und Auto ist ein Item
+    Item ist eine abstrakte Klasse welche zu einem Soldat 'assigned' ist
+        - wenn ein Item usable als false hat kann man es nicht zu einen
+          Soldat zuordnen
+    Ein Battalion enthalt mehrere Soldaten
+    Einen Vehicle kann mehrere Fahrer haben, ein Soldat kann mehrere Vehicles fahren
+    In der Registration wird sich eine Person als Soldat registrieren,
+    wird random zu einer Battalion zugeordnet und bekommt Waffe und Rustung.
+* */
