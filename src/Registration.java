cd5870aa27f63e4d79db8dba98e1fc30a6018e66
SofarRadu <sofarradu@yahoo.com>
2022-10-25 11:03:30 +0300

    Registration text

--- src/Registration.java
+++ src/Registration.java
@@ -14,9 +14,13 @@ public class Registration {
     Item ist eine abstrakte Klasse welche zu einem Soldat 'assigned' ist
         - wenn ein Item usable als false hat kann man es nicht zu einen
           Soldat zuordnen
-    Ein Battalion enthalt mehrere Soldaten
+    Ein Battalion enthalt mehrere Soldaten und ein leader
     Einen Vehicle kann mehrere Fahrer haben, ein Soldat kann mehrere Vehicles fahren
     In der Registration wird sich eine Person als Soldat registrieren,
     wird random zu einer Battalion zugeordnet und bekommt Waffe und Rustung.
+    Inventory hat eine liste fur Waffe, Rustung und Vehicles, aus dieser wird der Soldat der sich
+    registriert eine Waffe und Rustung welche nicht zugeordnet sind.
+    Jeder Soldat hat eine Stat-Klasse welche ihre Fahigkeiten verspiegelt.
+
 
 * */
