1f6d1e49121cdf183ff4f3c8c22497aae4603816
nggyg <andra.ulesan@gmail.com>
2022-10-25 11:42:43 +0300

    now I know how to work with files!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import basic.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            File in = new File("data.txt");
+            Scanner scan = new Scanner(in);
+            System.out.println(scan.next());
+            scan.close();
+        }catch (FileNotFoundException e){
+            System.out.println("File error");
+            e.printStackTrace();
+        }
     }
 }
