cb80a9990d826b7e4d97a11a12beca76cca04378
SofarRadu <sofarradu@yahoo.com>
2022-11-01 13:33:15 +0200

    Armor parametru nou Teste la ItemController aproape gata

--- src/Tests/ItemControllerTest.java
+++ src/Tests/ItemControllerTest.java
@@ -1,8 +1,11 @@
 package Tests;
 
 import Controller.ItemController;
+import basic.Soldier;
 import Repository.Repository;
+import basic.Armor;
 import basic.Item;
+import basic.Vehicle;
 import basic.Weapon;
 import org.junit.jupiter.api.Test;
 
@@ -16,24 +19,52 @@ class ItemControllerTest {
     void add() {
         ItemController itemController = new ItemController(new Repository<Item>(new ArrayList<Item>()));
         Weapon weapon = new Weapon("AK-74", (float) 7.62,1);
-        itemController.add(weapon);
-        assert(itemController.findById(1)==weapon);
+        Armor armor = new Armor("Heavy","Heavy",2);
+        Vehicle vehicle = new Vehicle("Humvee",3,new ArrayList<Soldier>());
 
+        itemController.add(weapon);
+        itemController.add(armor);
+        itemController.add(vehicle);
+        assert(itemController.findById(1)==weapon);
+        assert(itemController.findById(2)==armor);
+        assert (itemController.findById(3)==vehicle);
     }
 
     @Test
     void removeItem() {
         ItemController itemController = new ItemController(new Repository<Item>(new ArrayList<Item>()));
         Weapon weapon = new Weapon("AK-74", (float) 7.62,1);
+        Armor armor = new Armor("Heavy","Heavy",2);
+        Vehicle vehicle = new Vehicle("Humvee",3,new ArrayList<Soldier>());
+
         itemController.add(weapon);
+        itemController.add(armor);
+        itemController.add(vehicle);
         assert(itemController.removeItem(1));
         assert(!itemController.removeItem(1));
         assert(itemController.findById(1)==null);
+
+        assert(itemController.removeItem(2));
+        assert(!itemController.removeItem(2));
+        assert(itemController.findById(2)==null);
+
+        assert(itemController.removeItem(3));
+        assert(!itemController.removeItem(3));
+        assert(itemController.findById(3)==null);
 
     }
 
     @Test
     void update() {
+        ItemController itemController = new ItemController(new Repository<Item>(new ArrayList<Item>()));
+        Weapon weapon = new Weapon("AK-74", (float) 7.62,1);
+        Armor armor = new Armor("Heavy","Heavy",2);
+        Vehicle vehicle = new Vehicle("Humvee",3,new ArrayList<Soldier>());
+
+        itemController.add(weapon);
+        itemController.add(armor);
+        itemController.add(vehicle);
+
     }
 
     @Test
