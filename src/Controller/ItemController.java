110fa48be5774902c3a8447d1445ec453e72dca5
nggyg <andra.ulesan@gmail.com>
2022-11-08 11:06:35 +0200

    patched up itemcontroller, mostly works

--- src/Controller/ItemController.java
+++ src/Controller/ItemController.java
@@ -10,13 +10,10 @@ public class ItemController {
     private Repository<Armor> armors;
     private Repository<Vehicle> vehicles;
     public ItemController(Repository<Item> content) {
-        this.content = content;
-//        this.weapons = new Repository<Weapon>(new ArrayList<Weapon>());
-//        this.armors = new Repository<Armor>(new ArrayList<Armor>());
-//        this.vehicles = new Repository<Vehicle>(new ArrayList<Vehicle>());
+        this.weapons = new Repository<Weapon>(new ArrayList<Weapon>());
+        this.armors = new Repository<Armor>(new ArrayList<Armor>());
+        this.vehicles = new Repository<Vehicle>(new ArrayList<Vehicle>());
     }
-
-    private Repository<Item> content;
     public Item findById(int id) {
         for (Item item : weapons.getContent()) {
             if (item.getID() == id) {
@@ -44,7 +41,7 @@ public class ItemController {
             this.vehicles.add((Vehicle) newItem);
     }
     public boolean removeItem(int id){
-        Object foundItem=this.findById(id);
+        Item foundItem=this.findById(id);
         if(foundItem==null)
             return false;
         if(foundItem.getClass()==Armor.class){
@@ -62,23 +59,26 @@ public class ItemController {
         return false;
     }
     public boolean update(int id){
-        for(Item item:this.content.getContent()) {
-            if (item.getID() == id) {
-                if(item.getClass()==Armor.class){
-                    //what?
-                }
-                else if(item.getClass()== Weapon.class){
-                    //the
-                }
-                else if(item.getClass()== Vehicle.class){
-                    //f*ck?
-                }
-                return true;
+        Item foundItem=this.findById(id);
+        if(foundItem==null)
+            return false;
+        if (foundItem.getID() == id) {
+            if(foundItem.getClass()==Armor.class){
+                //what?
             }
+            else if(foundItem.getClass()== Weapon.class){
+                //the
+            }
+            else if(foundItem.getClass()== Vehicle.class){
+                //f*ck?
+            }
+            return true;
         }
         return false;
     }
     public void display(){
-        this.content.display();
+        this.weapons.display();
+        this.armors.display();
+        this.vehicles.display();
     }
 }
