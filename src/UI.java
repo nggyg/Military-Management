3bcfe9a80629a1258f6686d280f6bc487c3c2b10
nggyg <andra.ulesan@gmail.com>
2022-11-15 11:30:46 +0200

    UI only one

--- src/UI.java
+++ src/UI.java
@@ -69,22 +69,5 @@ public class UI{
         }
     }
 }
-import Controller.*;
-import basic.Armor;
-import basic.Soldier;
-import basic.Vehicle;
-import basic.Weapon;
 
-import java.util.ArrayList;
 
-public class UI{
-    public UI(){
-    ItemController itemController=new ItemController();
-    SoldierController soldierController = new SoldierController();
-
-    Weapon weapon = new Weapon("AK-74", (float) 7.62,1);
-    Armor armor = new Armor("Heavy","Heavy",2);
-    Vehicle vehicle = new Vehicle("Humvee",3,new ArrayList<Soldier>());
-
-    }
-}
